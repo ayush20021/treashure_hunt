e78843d714edaabc3c3a04a90cd9c6f448f48981
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2025-05-01 13:44:08 +0530

    Completed Login and Sign up Part

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\Users;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+
+Route::post('/create-account', [Users::class, 'createAccount'])->name('create-account');
+
+
+Route::get('/dashboard',function (){
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
+
+
+Route::post('/authenticate-user', [Users::class, 'authenticate'])->name('authenticate-user');
+
+
+Route::get('/login',function (){
+    return view('login');
+})->name('login');
+
+
+
+
